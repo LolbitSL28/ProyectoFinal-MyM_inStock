f34be29080b46c0255c23902503fdde07a2b11bc
Bit <hannitah28@gmail.com>
2026-05-23 18:55:29 -0700

    AppDbContext creation and setting
    AppDbContext including all tables

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyM_inStock.Models;
+
+namespace MyM_inStock.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Producto> Productos { get; set; }
+        public DbSet<Proveedor> Proveedores { get; set; }
+        public DbSet<Compra> Compras { get; set; }
+        public DbSet<DetalleCompra> DetallesCompra { get; set; }
+        public DbSet<Venta> Ventas { get; set; }
+        public DbSet<DetalleVenta> DetallesVenta { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+    }
+}
