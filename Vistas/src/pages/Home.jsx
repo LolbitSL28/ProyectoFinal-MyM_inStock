81d7592dc8007a5b156b1823405f73da8519968c
RubensFut <rubensfut6@gmail.com>
2026-05-27 10:06:34 -0700

    Primer commit css
    Ya me ando acordando como sirve css yippee

--- Vistas/src/pages/Home.jsx
+++ Vistas/src/pages/Home.jsx
@@ -1,8 +1,13 @@
+import './Home.css';
 function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-      <p>Esta es la pagina de inicio</p>
+    <div className="home-container">
+      <header className="home-hero">
+        <div className="hero-content">
+          <h1>MyM inStock</h1>
+          <p>calando el home a ver si me acuerdo del css</p>
+        </div>
+      </header>
     </div>
   );
 }
