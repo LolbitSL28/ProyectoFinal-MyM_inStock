4519012a8836755300cfe053b29cce22954fbecc
Issac-the-creator <roblescarrillo.issac@gmail.com>
2026-05-29 05:41:52 -0700

    agregar Ventas.css y aplicar en GetVentas y DetalleVentas

--- Vistas/src/pages/Ventas/GetVentas.jsx
+++ Vistas/src/pages/Ventas/GetVentas.jsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { listarVentas } from "../../redux/actions/ventasActions";
 import { useNavigate } from "react-router-dom";
 import TableVentas from "./TableVentas";
+import "./Ventas.css";
 
 function GetVentas() {
   const dispatch = useDispatch();
@@ -13,14 +14,16 @@ function GetVentas() {
     dispatch(listarVentas());
   }, [dispatch]);
 
-  return (
-    <>
-      <h1>Ventas</h1>
-      <button onClick={() => dispatch(listarVentas())}>Actualizar</button>
-      <button onClick={() => navigate("/ventas/add")}>Nueva Venta</button>
-      <TableVentas ventas={ventas} />
-    </>
-  );
+ return (
+  <div className="ventas-container">
+    <h1>Ventas</h1>
+    <div className="action-bar">
+      <button className="btn-action btn-refresh" onClick={() => dispatch(listarVentas())}>Actualizar</button>
+      <button className="btn-action btn-add" onClick={() => navigate("/ventas/add")}>Nueva Venta</button>
+    </div>
+    <TableVentas ventas={ventas} />
+  </div>
+);
 }
 
 export default GetVentas;
